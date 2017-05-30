2f0df9e9ef34a4d88f25a6145c58f0034a91b0a4
Joshua Egan <joshuae@ifit.com>
2017-05-30 15:57:20 -0600

    add test

--- src/generators.specs.js
+++ src/generators.specs.js
@@ -6,4 +6,8 @@ describe('generators', () => {
   it('should run tests', () => {
     expect(true).to.equal(true);
   });
+
+  it('should fail a test', () => {
+    expect(false).to.equal(true);
+  });
 });
