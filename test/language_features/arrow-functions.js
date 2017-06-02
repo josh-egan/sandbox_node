511ffce341a058b87630ffc16e868ea112128b93
Joshua Egan <joshuae@ifit.com>
2017-06-02 14:56:18 -0600

    tests for `this` with functions and arrow functions

--- test/language_features/arrow-functions.js
+++ test/language_features/arrow-functions.js
@@ -2,6 +2,13 @@
 
 const expect = require('chai').expect;
 
+/*
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+ An arrow function expression has a shorter syntax than a function expression and does not bind
+ its own this, arguments, super, or new.target. These function expressions are best suited
+ for non-method functions, and they cannot be used as constructors.
+ */
 describe('arrow functions', () => {
   it('should not require brackets for a one liner and should return the result on the right of the arrow', () => {
     const test = () => 'such awesome';
@@ -21,16 +28,6 @@ describe('arrow functions', () => {
     expect(test(9, 2, 5)).to.eql(16);
   });
 
-  it('should capture the parent function context', function() {
-    this.foo = 'bar';
-
-    const control = function() { return this && this.foo; };
-    const test = () => this.foo;
-
-    expect(control()).to.eql(undefined);
-    expect(test()).to.eql('bar');
-  });
-
   it('should support bracketed multi-line function', () => {
     const test = (x, y) => {
       const z = x + y;
@@ -38,5 +35,32 @@ describe('arrow functions', () => {
     };
 
     expect(test(10, 20)).to.eql(15);
+  });
+
+  it('should capture `this` from the parent function', function() {
+    const control = function() {
+      this.foo = 'kelp';
+      this.method = function() { return this.foo; }
+    };
+    const test = function() {
+      this.foo = 'bar';
+      this.method = () => this.foo;
+    };
+
+    expect(new control().method()).to.eql('kelp');
+    expect(new test().method()).to.eql('bar');
+  });
+
+  it('should capture `arguments` from the parent function', () => {
+    const control = function() {
+      return Array.prototype.slice.call(arguments);
+    };
+    const test = function() {
+      const arrowFn = () => Array.prototype.slice.call(arguments);
+      return arrowFn(1, 2, 3, 4, 5);
+    };
+
+    expect(control(6, 7, 8)).to.eql([6, 7, 8]);
+    expect(test(9, 10, 11)).to.eql([9, 10, 11]);
   });
 });
---
+++ test/language_features/functions.js
@@ -0,0 +1,43 @@
+// Note that use strict is intentionally not placed at the top of this file.
+
+const expect = require('chai').expect;
+
+function globalFunctionStrict() {
+  'use strict';
+  return this;
+}
+
+function globalFunction() {
+  return this;
+}
+
+describe('functions', () => {
+
+  /*
+   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+
+   In the global execution context (outside of any function), this refers to the global object, whether in strict mode or not.
+   */
+  context('this keyword', () => {
+    it('a function NOT in strict mode should return the global object as this', () => {
+      function localFunction() {
+        return this;
+      }
+
+      expect(globalFunction()).to.eql(global);
+      expect(localFunction()).to.eql(global);
+    });
+
+    it('a function in strict mode will have a this of undefined unless newed up in which case it will get its own this object', () => {
+      function localFunctionStrict() {
+        'use strict';
+        return this;
+      }
+
+      expect(globalFunctionStrict()).to.be.undefined;
+      expect(localFunctionStrict()).to.be.undefined;
+      expect(new globalFunctionStrict()).to.be.eql({});
+      expect(new localFunctionStrict()).to.be.eql({});
+    });
+  });
+});
