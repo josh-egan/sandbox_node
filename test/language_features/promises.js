1af51740879ba72e5bd6b809073afe6a86814ce5
Joshua Egan <joshuae@ifit.com>
2017-07-17 11:09:47 -0600

    add promise tests

---
+++ test/language_features/promises.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('promises', () => {
+  describe('node native promises', () => {
+    describe('#Promise.resolve', () => {
+      it('should resolve the promise with the value provided', () => {
+        return Promise.resolve('foo')
+          .then(result => expect(result).to.equal('foo'));
+      });
+
+      it('should resolve if given an error', () => {
+        return Promise.resolve(new Error('ooops'))
+          .then(result => expect(result.message).to.equal('ooops'));
+      });
+
+      it('should resolve a function and return the result', () => {
+        return Promise.resolve(() => 1234)
+          .then(result => expect(result).to.be.a('function'));
+      });
+    });
+
+    describe('#Promise.reject', () => {
+      it('should reject when given a primitive type', () => {
+        return Promise.reject(1234)
+          .catch(err => expect(err).to.equal(1234));
+      });
+
+      it('should reject a function', () => {
+        return Promise.reject(() => 1234)
+          .catch(err => expect(err).to.be.a('function'));
+      });
+
+      it('should reject with an error', () => {
+        return Promise.reject(new Error('asdf'))
+          .catch(err => expect(err.message).to.equal('asdf'));
+      });
+    });
+
+    describe('#Promise constructor', () => {
+      it('should work with the resolve callback', () => {
+        return new Promise((resolve, reject) => resolve('foo'))
+          .then(result => expect(result).to.equal('foo'));
+      });
+
+      it('should work with the reject callback', () => {
+        return new Promise((resolve, reject) => reject(new Error('hjk')))
+          .catch(err => expect(err.message).to.equal('hjk'));
+      });
+
+      it('should reject if an error is thrown inside of the function', () => {
+        let error;
+        return new Promise((resolve, reject) => {
+          const randValue = Math.random();
+          if (randValue < 1)
+            throw new Error('What, this library does what it is supposed to do? Got a random value of: ' + randValue);
+          resolve();
+        })
+          .catch(err => error = err)
+          .then(() => expect(error.message).to.match(/What, this library does what it is supposed to do/));
+      });
+    });
+
+    describe('#.then', () => {
+      it('should take in the result of the resolved promise', () => {
+        return new Promise((resolve, reject) => resolve('asdf'))
+          .then(result => expect(result).to.equal('asdf'));
+      });
+
+      it('should also be able to handle the error with a second callback', () => {
+        return new Promise((resolve, reject) => reject(new Error('yui')))
+          .then(result => {}, err => expect(err.message).to.equal('yui'));
+      });
+
+      it('should execute successive then callbacks', () => {
+        return new Promise((resolve, reject) => resolve('foo'))
+          .then(result => expect(result).to.equal('foo'))
+          .then(result => expect(result).to.be.an('object'))
+      });
+    });
+  });
+});
